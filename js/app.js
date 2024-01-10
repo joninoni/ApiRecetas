40abfe3381ce1ce002ae07fd0035cbf9d5f5150c
joninoni <jonibeltranhayer@gmail.com>
2024-01-10 00:19:33 -0600

    obteniendo y mostrando las categorias en el select

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function iniciarApp(){
+    const selectCategorias =document.querySelector("#categorias");
+    obtenerCategorias();
+
+    function obtenerCategorias(){
+        const url=`https://www.themealdb.com/api/json/v1/1/categories.php`;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarCategorias(resultado.categories));
+    }
+
+    function mostrarCategorias(categorias =[]){
+        categorias.forEach( categoria => {
+            const option = document.createElement("option");
+            option.value = categoria.strCategory;
+            option.textContent= categoria.strCategory;
+            selectCategorias.appendChild(option);
+        })
+    }
+
+}
+document.addEventListener("DOMContentLoaded",iniciarApp);
